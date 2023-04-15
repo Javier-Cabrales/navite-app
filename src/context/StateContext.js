8f651a1156a47f69b75099130534ae4bc54548c8
javier-plus <javiereliascabraesromero@gmail.com>
2023-04-15 10:49:50 -0500

    changes to the initial state object new function and a reducer for state control and actions

--- src/context/StateContext.js
+++ src/context/StateContext.js
@@ -1,24 +1,49 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react"
 
 export const StateContext = createContext()
 
-const user = {
-    id: "",
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: ""
-}
-
 const initialState = {
-    user,
+    user: {
+        id: "",
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    },
     questions: [{ question: "", answere: "" }],
     isLoggedIn: false
 }
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "UPDATE_USER":
+            return { ...state, user: action.payload };
+        case "UPDATE_QUESTIONS":
+            return { ...state, questions: action.payload };
+        case "UPDATE_IS_LOGGED_IN":
+            return { ...state, isLoggedIn: action.payload }
+        default:
+            return state
+    }
+}
+
 const StateProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const updateUser = (newUser) => {
+        dispatch({ type: "UPDATE_USER", payload: newUser })
+    }
+
+    const updateQuestions = (newQuestions) => {
+        dispatch({ type: "UPDATE_QUESTIONS", payload: newQuestions })
+    }
+
+    const updateIsLoggedIn = (isLoggedIn) => {
+        dispatch({ type: "UPDATE_IS_LOGGED_IN", payload: isLoggedIn })
+    }
+
     return (
-        <StateContext.Provider value={{ initialState }}>
+        <StateContext.Provider value={{ state, updateUser, updateQuestions, updateIsLoggedIn }}>
             {children}
         </StateContext.Provider>
     )
